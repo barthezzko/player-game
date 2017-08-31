c952188691de1a5e74f29e2083f1cefe218ec64f
Baytsurov <MBaytsurov@luxoft.com>
2017-08-31 14:25:06 +0300

    working prototype for concurrent

--- src/main/java/com/barthezzko/playergame/multithread/MultiThreadedPlayGroundImpl.java
+++ src/main/java/com/barthezzko/playergame/multithread/MultiThreadedPlayGroundImpl.java
@@ -1,100 +1,79 @@
 package com.barthezzko.playergame.multithread;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
-import java.util.concurrent.Exchanger;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 
-import com.barthezzko.playergame.impl.MessageImpl;
-import com.barthezzko.playergame.impl.PlayerImpl;
 import com.barthezzko.playergame.model.Message;
-import com.barthezzko.playergame.model.MessageBus;
 import com.barthezzko.playergame.model.PlayGround;
-import com.barthezzko.playergame.model.Player;
 
 public class MultiThreadedPlayGroundImpl implements PlayGround {
 
 	private static final int PLAYER_NUMBER = 2; // TODO: move to config
-	private Random rnd = new Random();
-	private ExecutorService executorService = Executors.newFixedThreadPool(PLAYER_NUMBER);  
-	private List<Player> players = new ArrayList<>();
+	private ExecutorService executorService = Executors
+			.newFixedThreadPool(PLAYER_NUMBER);
 	private Logger logger = Logger.getLogger(MultiThreadedPlayGroundImpl.class);
-	private MessageBus mBus = new MultiThreadedBusImpl();
+	private volatile StringBuilder msgBuilder = new StringBuilder("start");
 
-	public void initialize(String startMessage) {
-		int initiatorPlayer = rnd.nextInt(PLAYER_NUMBER);
-		for (int i = 0; i < PLAYER_NUMBER; i++) {
-			Player player = new PlayerImpl(mBus, i);
-			players.add(player);
-			executorService.submit(()->{
-				mBus.register((message)->{
-					logger.info(message);
-				});
-			});
-			logger.info("Starting player [" + i + "]");
-		}
-		logger.info("Player [" + initiatorPlayer + "] starts");
-		Player initiator = players.get(initiatorPlayer);
-		Player another = players.get(initiatorPlayer == 1 ? 0 : 1);
-		mBus.send(new MessageImpl(startMessage, initiator, another));
-		sleep(10);
+	public static void main(String[] args) {
+		new MultiThreadedPlayGroundImpl().initialize("ole");
 	}
 
-	private void sleep(int i) {
+	public void initialize(String startMessage) {
+		for (int i = 0; i < PLAYER_NUMBER; i++) {
+			executorService.submit(new RunnablePlayer(i));
+			logger.info("Starting player [" + i + "]");
+		}
 		try {
-			TimeUnit.SECONDS.sleep(i);
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
-			logger.error(e);
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		synchronized (msgBuilder) {
+			msgBuilder.notify();
+		}
+	}
+
+	class RunnablePlayer implements Runnable {
+
+		private int invocationCounter;
+		private int playerID;
+
+		public RunnablePlayer(int playerID) {
+			this.playerID = playerID;
+		}
+
+		@Override
+		public void run() {
+			logger.info("Starting ["+playerID+"]");
+			while (invocationCounter < 11) {
+				synchronized (msgBuilder) {
+					try {
+						msgBuilder.wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					logger.info("[" + playerID + "] message:" + msgBuilder.toString());
+					msgBuilder.append(invocationCounter);
+					invocationCounter++;
+					logger.info("notifying");
+					msgBuilder.notify();
+				}
+			}
+			logger.info("player["+playerID+"] done");
+			synchronized (msgBuilder) {
+				msgBuilder.notify();
+			}
 		}
 	}
 
 	@Override
 	public List<Message> getMessages() {
-		return mBus.getMessages();
-	}
-
-	class PlayerWrapper extends PlayerImpl implements Runnable{
-
-		public PlayerWrapper(MessageBus mBus, int playerID) {
-			super(mBus, playerID);
-		}
-
-		@Override
-		public void run() {
-			/*while(finished){
-				mBus.send(message);
-			}*/
-		}
-		
-	}
-	
-	
-	class MultiThreadedBusImpl implements MessageBus {
-
-		private final List<Message> messagesHistory = new ArrayList<>();
-		
-		@Override
-		public void send(Message message) {
-			messagesHistory.add(message);
-			logger.info("Sending message " + message + "...");
-			message.getRecepient().onMessage(message);
-		}
-
-		@Override
-		public List<Message> getMessages() {
-			return Collections.unmodifiableList(messagesHistory);
-		}
-
-		@Override
-		public void register(Player recepient) {
-			
-		}
-		
+		return Collections.emptyList();
 	}
 }
