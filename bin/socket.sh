14fefd13bc43a9d9c7eb0d5687f9ed9276d2eb13
bzpc\barthezzko <barthezzko@gmail.com>
2017-09-09 22:50:38 -0400

    linux version enhanced with wait

--- bin/socket.sh
+++ bin/socket.sh
@@ -1,2 +1,4 @@
 java -classpath "../dist/player-game-1.0-SNAPSHOT-jar-with-dependencies.jar" com.barthezzko.playergame.runner.Runner socket-server 9090 &
+echo "Waiting for 5 seconds to init server..."
+timeout 5
 java -classpath "../dist/player-game-1.0-SNAPSHOT-jar-with-dependencies.jar" com.barthezzko.playergame.runner.Runner socket-client 9090
